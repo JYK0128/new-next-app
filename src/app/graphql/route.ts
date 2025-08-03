8b12631dd2bfd3def6faee9d32f084765193cd51
jyk0128 <wlsyong90@gmail.com>
2025-08-03 12:35:02 +0900

    dev. prisma settings

--- src/app/graphql/route.ts
+++ src/app/graphql/route.ts
@@ -3,6 +3,7 @@ import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import type { NextRequest } from "next/server";
 
 import { resolvers, typeDefs } from "@/graphql";
+import prisma from "@/lib/prisma";
 
 const server = new ApolloServer({
   resolvers,
@@ -10,7 +11,7 @@ const server = new ApolloServer({
 });
 
 const handler = startServerAndCreateNextHandler<NextRequest>(server, {
-  context: async (req) => ({ req }),
+  context: async (req) => ({ req, prisma }),
 });
 
 export { handler as GET, handler as POST };
