f2e2a2921ead159ff6aa9d5063766e453d9076d7
jyk0128 <wlsyong90@gmail.com>
2025-08-14 14:57:40 +0900

    Update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -1,18 +1,10 @@
 import NextAuth from "next-auth";
 import Keycloak from "next-auth/providers/keycloak";
 
-export interface AdapterUser {
-  id_token: string
-}
-
-export interface User {
-  id_token: string
-}
-
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [Keycloak],
   callbacks: {
-    async jwt({token, account}) {
+    async jwt({ token, account }) {
       if (account?.id_token) {
         token["id_token"] = account?.id_token;
       }
