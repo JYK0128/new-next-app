cb54b67ed885a1751817382f7cfded334e1c2eef
jyk0128 <wlsyong90@gmail.com>
2025-08-27 01:48:35 +0900

    Update route.ts

--- src/app/(server)/(api)/api/auth/login/route.ts
+++ src/app/(server)/(api)/api/auth/login/route.ts
@@ -3,10 +3,6 @@ import { NextResponse } from "next/server";
 import { signIn } from "@/auth";
 
 export async function GET() {
-  console.log("=====NEXT_SITE_DOMAIN=====", process.env.NEXT_SITE_DOMAIN);
-  console.log("=====AUTH_TRUST_HOST=====", process.env.AUTH_TRUST_HOST);
-  console.log("=====AUTH_URL=====", process.env.AUTH_URL);
-
   const res = await signIn("keycloak", {
     redirect: false,
     redirectTo: process.env.NEXT_SITE_DOMAIN,
