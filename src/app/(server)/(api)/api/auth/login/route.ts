827f3245b9d7ed2bed67d415c31fcf8b7f85f0d3
jyk0128 <wlsyong90@gmail.com>
2025-08-27 00:35:55 +0900

    dev

--- src/app/(server)/(api)/api/auth/login/route.ts
+++ src/app/(server)/(api)/api/auth/login/route.ts
@@ -3,13 +3,14 @@ import { NextResponse } from "next/server";
 import { signIn } from "@/auth";
 
 export async function GET() {
-  console.log("=========", process.env.NEXT_SITE_DOMAIN);
+  console.log("=====NEXT_SITE_DOMAIN=====", process.env.NEXT_SITE_DOMAIN);
+  console.log("=====AUTH_TRUST_HOST=====", process.env.AUTH_TRUST_HOST);
+  console.log("=====AUTH_URL=====", process.env.AUTH_URL);
 
   const res = await signIn("keycloak", {
     redirect: false,
     redirectTo: process.env.NEXT_SITE_DOMAIN,
   });
 
-  console.log("=========", res);
   return NextResponse.redirect(res);
 }
