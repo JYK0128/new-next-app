f2ecaf36509e69b296f58177cd4dc13af6fe86a8
jyk0128 <wlsyong90@gmail.com>
2025-08-27 09:32:36 +0900

    fix. Federated Logout

--- src/app/(server)/(api)/api/auth/logout/route.ts
+++ src/app/(server)/(api)/api/auth/logout/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
 
 export async function GET() {
   const session = await auth();
@@ -9,25 +9,18 @@ export async function GET() {
     return NextResponse.redirect(process.env.NEXT_SITE_URL);
   }
 
+  // auth 세션
+  await signOut({
+    redirect: false,
+    redirectTo: process.env.NEXT_SITE_URL,
+  });
+
+  // keycloak 세션
   const signoutURL = new URL(`${process.env.AUTH_KEYCLOAK_ISSUER}/protocol/openid-connect/logout`);
   signoutURL.searchParams.set("id_token_hint", session.user?.idToken);
   signoutURL.searchParams.set("post_logout_redirect_uri", process.env.NEXT_SITE_URL);
 
   const response = NextResponse.redirect(signoutURL);
-  response.cookies.delete("authjs.session-token");
-  response.cookies.delete({
-    name: "__Secure-authjs.session-token",
-    path: "/",
-    domain: new URL(process.env.NEXT_SITE_URL).hostname,
-    secure: true,
-  });
-
-  console.log({
-    name: "__Secure-authjs.session-token",
-    path: "/",
-    domain: new URL(process.env.NEXT_SITE_URL).hostname,
-    secure: true,
-  });
 
   return response;
 }
