5f6f0098c81e0a96fb438097e77a493fd93477b8
jyk0128 <wlsyong90@gmail.com>
2025-09-01 00:25:43 +0900

    Update AspectImage.tsx

--- src/components/custom/AspectImage.tsx
+++ src/components/custom/AspectImage.tsx
@@ -5,18 +5,18 @@ import { cn } from "@/lib/utils";
 
 
 type Props = Omit<ImageProps, "src" | "alt">
-  & { src: Nullish<string>, alt?: Nullish<string>, ratio?: number };
+  & { src: Nullish<string>, alt?: Optional<string>, ratio?: number };
 
-export function AspectImage({ src, alt, ratio, ...props }: Props) {
+export function AspectImage({ src, alt, ratio, priority, fill, ...props }: Props) {
   const orgSrc = src ? new URL(src, location.origin).toString() : "/no-image.svg";
 
   return (
     <AspectRatio ratio={ratio || (16 / 9)}>
       <Image
         src={orgSrc}
-        alt={alt || "no-image"}
-        priority={!src || src.startsWith("http")}
-        fill
+        alt={alt ?? "no-image"}
+        priority={priority ?? !src}
+        fill={fill ?? true}
         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
         {...props}
         className={cn("tw:object-contain", props.className)}
