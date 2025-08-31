68d00d6ce700a7068d3848bb32fffe1762788a48
jyk0128 <wlsyong90@gmail.com>
2025-09-01 00:15:19 +0900

    fix. 썸네일 파일경로2

--- src/components/custom/AspectImage.tsx
+++ src/components/custom/AspectImage.tsx
@@ -3,19 +3,21 @@ import Image, { type ImageProps } from "next/image";
 import { AspectRatio } from "@/components/shadcn";
 import { cn } from "@/lib/utils";
 
-export function AspectImage({ src, alt, ratio, ...props }: Omit<ImageProps, "src" | "alt">
-  & { src: Nullish<string>, alt?: Nullish<string>, ratio?: number }) {
+
+type Props = Omit<ImageProps, "src" | "alt">
+  & { src: Nullish<string>, alt?: Nullish<string>, ratio?: number };
+
+export function AspectImage({ src, alt, ratio, ...props }: Props) {
+  const orgSrc = src ? new URL(src, location.origin).toString() : "/no-image.svg";
+
   return (
     <AspectRatio ratio={ratio || (16 / 9)}>
       <Image
-        src={src || "/no-image.svg"}
+        src={orgSrc}
         alt={alt || "no-image"}
         priority={!src || src.startsWith("http")}
         fill
         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-        onError={(e) => {
-          e.currentTarget.src = "/no-image.svg";
-        }}
         {...props}
         className={cn("tw:object-contain", props.className)}
       />
