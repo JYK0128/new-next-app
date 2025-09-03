50c41bf14c5f55594447b57ac17a1481815254f0
jyk0128 <wlsyong90@gmail.com>
2025-09-03 14:46:26 +0900

    dev. 일반 scroll 예시

--- src/app/[locale]/(public)/page.tsx
+++ src/app/[locale]/(public)/page.tsx
@@ -1,49 +1,76 @@
 "use client";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { SplitText } from "gsap/SplitText";
+import { ScrollTrigger, SplitText } from "gsap/all";
 import { useTranslations } from "next-intl";
 import { useRef } from "react";
 
 import { InquiryForm } from "@/app/[locale]/(public)/inquiry-form";
-import { cn } from "@/lib/utils";
 
+
+gsap.registerPlugin(useGSAP, ScrollTrigger, SplitText);
 
 export default function Page() {
   const t = useTranslations();
-  const ref = useRef<HTMLDivElement | null>(null);
+  const textRef = useRef<HTMLDivElement | null>(null);
+  const screenRef = useRef<HTMLDivElement | null>(null);
 
   useGSAP(() => {
-    if (!ref.current) return;
-    gsap.registerPlugin(SplitText);
+    if (!textRef.current) return;
+    textRef.current.classList.remove("tw:invisible");
 
-    const split = new SplitText(ref.current, { type: "chars, words" });
-
-    ref.current.classList.toggle("tw:invisible");
-    gsap.from(split.chars, {
-      opacity: 0,
-      y: 20,
-      stagger: 0.05,
-      ease: "power3.out",
-      duration: 0.6,
+    const split = SplitText.create(textRef.current, {
+      type: "chars, words",
+      onSplit: (self) => {
+        gsap.from(self.chars, {
+          opacity: 0,
+          y: 20,
+          stagger: 0.05,
+          ease: "power3.out",
+          duration: 0.6,
+        });
+      },
     });
 
     return () => {
       split.revert();
-      gsap.killTweensOf(split.chars);
+    };
+  });
+
+  useGSAP(() => {
+    if (!screenRef.current) return;
+    const { scrollHeight, clientHeight, children } = screenRef.current;
+
+    const scroll = ScrollTrigger.create({
+      trigger: screenRef.current,
+      scroller: screenRef.current,
+      start: "top top",
+      end: scrollHeight - clientHeight,
+      scrub: true,
+      snap: {
+        snapTo: 1 / (children.length - 1),
+        duration: 1,
+        ease: "power4.inOut",
+      },
+
+      markers: true,
+    });
+
+    return () => {
+      scroll.kill();
     };
   });
 
 
   return (
-    <div className={cn(
-      "tw:size-full tw:scroll-y",
-    )}
-    >
-      <div ref={ref} className="tw:size-full tw:m-auto tw:invisible">
+    <div ref={screenRef} className="tw:size-full tw:scroll-y">
+      <div ref={textRef} className="tw:size-full tw:m-auto tw:invisible">
         {t("welcome.title")}
         <br />
         {t("welcome.message", { name: t("name") })}
+      </div>
+      <div className="tw:size-full tw:m-auto">
+        가난하고, 배고픈 상태에요.
       </div>
       <div className="tw:size-full tw:m-auto">
         <InquiryForm />
