8590ce9cccdc384037eea203237da5b27ef8e433
jyk0128 <wlsyong90@gmail.com>
2025-09-12 10:17:43 +0900

    fix. no-image 문제

--- src/components/custom/AspectImage.tsx
+++ src/components/custom/AspectImage.tsx
@@ -1,4 +1,5 @@
 import Image, { type ImageProps } from "next/image";
+import type { SyntheticEvent } from "react";
 
 import { AspectRatio } from "@/components/shadcn";
 import { cn } from "@/lib/utils";
@@ -7,7 +8,7 @@ import { cn } from "@/lib/utils";
 type Props = Omit<ImageProps, "src" | "alt">
   & { src: Nullish<string>, alt?: Optional<string>, ratio?: number };
 
-export function AspectImage({ src, alt, ratio, priority, fill, ...props }: Props) {
+export function AspectImage({ src, alt, ratio, priority, fill, onError, ...props }: Props) {
   const orgSrc = src ?? "/no-image.svg";
 
   return (
@@ -17,6 +18,10 @@ export function AspectImage({ src, alt, ratio, priority, fill, ...props }: Props
         alt={alt ?? "no-image"}
         priority={priority ?? !src}
         fill={fill ?? true}
+        onError={(evt) => {
+          onError?.(evt);
+          (evt.target as HTMLImageElement).src = "/no-image.svg";
+        }}
         {...props}
         className={cn("tw:object-contain", props.className)}
       />
